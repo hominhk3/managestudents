4d80cf67baa198b86daa716be2ef513690c288bf
hominhk3 <144079318+hominhk3@users.noreply.github.com>
2023-09-19 09:28:49 +0700

    Add files via upload

---
+++ managestudents/src/view/Main.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package view;
+
+import common.Validation;
+import controller.Manager;
+import java.util.ArrayList;
+import model.Student;
+
+/**
+ *
+ * @author dell
+ */
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Student> ls = new ArrayList<>();
+        Validation validation = new Validation();
+        ls.add(new Student("1", "Pham Ngoc Hoa", "Spring", "java"));
+        ls.add(new Student("2", "Do Quang Hiep", "Summer", ".net"));
+        ls.add(new Student("3", "Nguyen Xuan Cuong", "Spring", "c/c++"));
+        int count = 3;
+        //loop until user want to exit program
+        while (true) {
+            //Show menu option
+            Manager.menu();
+            int choice = validation.checkInputIntLimit(1, 5);
+            switch (choice) {
+                case 1:
+                    Manager.createStudent(count, ls);
+                    break;
+                case 2:
+                    Manager.findAndSort(ls);
+                    break;
+                case 3:
+                    Manager.updateOrDelete(count, ls);
+                    break;
+                case 4:
+                    Manager.report(ls);
+                    break;
+                case 5:
+                    return;
+            }
+
+        }
+    }
+}
